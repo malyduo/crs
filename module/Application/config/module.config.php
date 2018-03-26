f002fda176a096d942a6d85ab07931ba0cb5d3e3
kmaciag <kmaciag@montownia-stron.pl>
2018-03-26 22:02:19 +0200

    rezerwacja styl + liczenie dni dat kalendarza cennik

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
  * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
@@ -17,96 +18,106 @@ return [
             'home' => [
                 'type' => Literal::class,
                 'options' => [
-                    'route'    => '/',
+                    'route' => '/',
                     'defaults' => [
                         'controller' => Controller\IndexController::class,
-                        'action'     => 'index',
+                        'action' => 'index',
                     ],
                 ],
             ],
             'kontakt' => [
                 'type' => Literal::class,
                 'options' => [
-                    'route'    => '/kontakt',
+                    'route' => '/kontakt',
                     'defaults' => [
                         'controller' => Controller\IndexController::class,
-                        'action'     => 'kontakt',
+                        'action' => 'kontakt',
                     ],
                 ],
             ],
             'samochody' => [
                 'type' => Literal::class,
                 'options' => [
-                    'route'    => '/samochody',
+                    'route' => '/samochody',
                     'defaults' => [
                         'controller' => Controller\IndexController::class,
-                        'action'     => 'samochody',
+                        'action' => 'samochody',
                     ],
                 ],
             ],
             'regulamin' => [
                 'type' => Literal::class,
                 'options' => [
-                    'route'    => '/regulamin',
+                    'route' => '/regulamin',
                     'defaults' => [
                         'controller' => Controller\IndexController::class,
-                        'action'     => 'regulamin',
+                        'action' => 'regulamin',
                     ],
                 ],
             ],
-            'application' => [
-                'type'    => Segment::class,
-                'options' => [
-                    'route'    => '/application[/:action]',
-                    'defaults' => [
-                        'controller' => Controller\IndexController::class,
-                        'action'     => 'index',
-                    ],
-                ],
-            ],
+//            'application' => [
+//                'type'    => Segment::class,
+//                'options' => [
+//                    'route'    => '/application[/:action]',
+//                    'defaults' => [
+//                        'controller' => Controller\IndexController::class,
+//                        'action'     => 'index',
+//                    ],
+//                ],
+//            ],
             'rezerwacja' => [
-                'type'    => Segment::class,
+                'type' => Segment::class,
                 'options' => [
-                    'route'    => '/rezerwacja[/:action]',
+                    'route' => '/rezerwacja[/:action]',
                     'defaults' => [
                         'controller' => Controller\RezerwacjaController::class,
-                        'action'     => [
+                        'action' => [
                             'index',
                             'rezerwuj'
                         ],
                     ],
                 ],
             ],
+//            'flota' => [
+//                'type'    => Segment::class,
+//                'options' => [
+//                    'route'    => '/flota[/:action]',
+//                    'defaults' => [
+//                        'controller' => Controller\FlotaController::class,
+//                        'action'     => [
+//                            'index',
+//                            'szukaj'
+//                        ],
+//                    ],
+//                ],
+//            ],
+//            'flota' => [
+//                'type' => Literal::class,
+//                'options' => [
+//                    'route'    => '/flota',
+//                    'defaults' => [
+//                        'controller' => Controller\FlotaController::class,
+//                        'action'     => 'index',
+//                    ],
+//                ],
+//            ],
+//            'flota' => [
+//                'type' => Literal::class,
+//                'options' => [
+//                    'route'    => '/flota/szukaj',
+//                    'defaults' => [
+//                        'controller' => Controller\FlotaController::class,
+//                        'action'     => 'szukaj',
+//                    ],
+//                ],
+//            ],
             'flota' => [
-                'type'    => Segment::class,
+                'type' => Segment::class,
                 'options' => [
-                    'route'    => '/flota[/:action]',
+                    'route' => '/flota[/:action]',
                     'defaults' => [
                         'controller' => Controller\FlotaController::class,
-                        'action'     => [
-                            'index',
-                            'szukaj'
-                        ],
-                    ],
-                ],
-            ],
-            'flota' => [
-                'type' => Literal::class,
-                'options' => [
-                    'route'    => '/flota',
-                    'defaults' => [
-                        'controller' => Controller\FlotaController::class,
-                        'action'     => 'index',
-                    ],
-                ],
-            ],
-            'flota' => [
-                'type' => Literal::class,
-                'options' => [
-                    'route'    => '/flota/szukaj',
-                    'defaults' => [
-                        'controller' => Controller\FlotaController::class,
-                        'action'     => 'szukaj',
+                        'action' => 'index',
                     ],
                 ],
             ],
@@ -121,15 +132,15 @@ return [
     ],
     'view_manager' => [
         'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
+        'display_exceptions' => true,
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
         'template_map' => [
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+            'error/404' => __DIR__ . '/../view/error/404.phtml',
+            'error/index' => __DIR__ . '/../view/error/index.phtml',
         ],
         'template_path_stack' => [
             __DIR__ . '/../view',
